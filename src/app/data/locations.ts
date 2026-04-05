f77ef4b9cb518fffd7bc4f8ee274020006a28e98
Lorenzo Bisello <bisius98@gmail.com>
2026-04-05 09:30:18 +0200

    feat: add locations and materials data files

---
+++ src/app/data/locations.ts
@@ -0,0 +1,79 @@
+export enum LocationType {
+	City = 0,
+	Station = 1
+}
+
+export interface Location {
+	name: string
+	system: string
+	type: LocationType
+	refinery: boolean
+}
+
+export const locations: Location[] = [
+
+	// Crusader
+	{ name: 'Orison',            system: 'Stanton', type: LocationType.City,    refinery: false },
+	{ name: 'Seraphim Station',  system: 'Stanton', type: LocationType.Station, refinery: false },
+	{ name: 'CRU-L1',            system: 'Stanton', type: LocationType.Station, refinery: true  },
+	{ name: 'CRU-L2',            system: 'Stanton', type: LocationType.Station, refinery: false },
+	{ name: 'CRU-L3',            system: 'Stanton', type: LocationType.Station, refinery: false },
+	{ name: 'CRU-L4',            system: 'Stanton', type: LocationType.Station, refinery: false },
+	{ name: 'CRU-L5',            system: 'Stanton', type: LocationType.Station, refinery: false },
+
+	// Microtech
+	{ name: 'New Babbage',       system: 'Stanton', type: LocationType.City,    refinery: false },
+	{ name: 'Port Tressler',     system: 'Stanton', type: LocationType.Station, refinery: false },
+	{ name: 'MIC-L1',            system: 'Stanton', type: LocationType.Station, refinery: true  },
+	{ name: 'MIC-L2',            system: 'Stanton', type: LocationType.Station, refinery: true  },
+	{ name: 'MIC-L3',            system: 'Stanton', type: LocationType.Station, refinery: false },
+	{ name: 'MIC-L4',            system: 'Stanton', type: LocationType.Station, refinery: false },
+	{ name: 'MIC-L5',            system: 'Stanton', type: LocationType.Station, refinery: true  },
+
+	// ArcCorp
+	{ name: 'Area18',            system: 'Stanton', type: LocationType.City,    refinery: false },
+	{ name: 'Baijini Point',     system: 'Stanton', type: LocationType.Station, refinery: false },
+	{ name: 'ARC-L1',            system: 'Stanton', type: LocationType.Station, refinery: true  },
+	{ name: 'ARC-L2',            system: 'Stanton', type: LocationType.Station, refinery: true  },
+	{ name: 'ARC-L3',            system: 'Stanton', type: LocationType.Station, refinery: false },
+	{ name: 'ARC-L4',            system: 'Stanton', type: LocationType.Station, refinery: true  },
+	{ name: 'ARC-L5',            system: 'Stanton', type: LocationType.Station, refinery: false },
+
+	// Hurston
+	{ name: 'Loreville',         system: 'Stanton', type: LocationType.City,    refinery: false },
+	{ name: 'Everus Harbor',     system: 'Stanton', type: LocationType.Station, refinery: false },
+	{ name: 'HUR-L1',            system: 'Stanton', type: LocationType.Station, refinery: true  },
+	{ name: 'HUR-L2',            system: 'Stanton', type: LocationType.Station, refinery: false },
+	{ name: 'HUR-L3',            system: 'Stanton', type: LocationType.Station, refinery: false },
+	{ name: 'HUR-L4',            system: 'Stanton', type: LocationType.Station, refinery: false },
+	{ name: 'HUR-L5',            system: 'Stanton', type: LocationType.Station, refinery: false },
+
+	// Pyro
+	{ name: 'Checkmate',                system: 'Pyro', type: LocationType.Station, refinery: true  },
+	{ name: 'Orbituary',                system: 'Pyro', type: LocationType.Station, refinery: true  },
+	{ name: 'Ruin Station',             system: 'Pyro', type: LocationType.Station, refinery: true  },
+	{ name: 'Megumi Refueling',         system: 'Pyro', type: LocationType.Station, refinery: false },
+	{ name: 'Dudley & Daughters',       system: 'Pyro', type: LocationType.Station, refinery: false },
+	{ name: 'Endgame',                  system: 'Pyro', type: LocationType.Station, refinery: false },
+	{ name: "Rat's Nest",               system: 'Pyro', type: LocationType.Station, refinery: false },
+	{ name: "Rod's Fuel'n Suppies",     system: 'Pyro', type: LocationType.Station, refinery: false },
+	{ name: 'Starlight Service Station',system: 'Pyro', type: LocationType.Station, refinery: false },
+	{ name: 'Patch City',               system: 'Pyro', type: LocationType.Station, refinery: false },
+
+	// Nyx
+	{ name: 'Levski',                        system: 'Nyx',     type: LocationType.City,    refinery: false },
+	{ name: "People's Service Station Lambda",system: 'Nyx',    type: LocationType.Station, refinery: false },
+	{ name: "People's Service Station Theta", system: 'Nyx',    type: LocationType.Station, refinery: false },
+	{ name: "People's Service Station Delta", system: 'Nyx',    type: LocationType.Station, refinery: false },
+	{ name: "People's Service Station Alpha", system: 'Nyx',    type: LocationType.Station, refinery: false },
+
+	// Gateways
+	{ name: 'Stanton - Nyx Gateway',         system: 'Stanton', type: LocationType.Station, refinery: false },
+	{ name: 'Stanton - Pyro Gateway',        system: 'Stanton', type: LocationType.Station, refinery: false },
+	{ name: 'Stanton - Terra Gateway',       system: 'Stanton', type: LocationType.Station, refinery: false },
+	{ name: 'Pyro - Stanton Gateway',        system: 'Pyro',    type: LocationType.Station, refinery: false },
+	{ name: 'Pyro - Nyx Gateway',            system: 'Pyro',    type: LocationType.Station, refinery: false },
+	{ name: 'Nyx - Stanton Gateway',         system: 'Nyx',     type: LocationType.Station, refinery: false },
+	{ name: 'Nyx - Pyro Gateway',            system: 'Nyx',     type: LocationType.Station, refinery: false },
+
+]
---
+++ src/app/data/materials.ts
@@ -0,0 +1,37 @@
+export interface Material {
+	name: string;
+	short: string;
+}
+
+export const materials: Material[] = [
+	{ name: 'Agricium',        short: 'AGC' },
+	{ name: 'Aluminum',        short: 'ALU' },
+	{ name: 'Aphorite',        short: 'APH' },
+	{ name: 'Aslarite',        short: 'ASL' },
+	{ name: 'Beradom',         short: 'BER' },
+	{ name: 'Beryl',           short: 'BRL' },
+	{ name: 'Carinite',        short: 'CAR' },
+	{ name: 'Copper',          short: 'COP' },
+	{ name: 'Corundum',        short: 'COR' },
+	{ name: 'Dolivine',        short: 'DOL' },
+	{ name: 'Gold',            short: 'GLD' },
+	{ name: 'Hadanite',        short: 'HAD' },
+	{ name: 'Hephaestanite',   short: 'HEP' },
+	{ name: 'Iron',            short: 'IRN' },
+	{ name: 'Janalite',        short: 'JAN' },
+	{ name: 'Laranite',        short: 'LAR' },
+	{ name: 'Lindinium',       short: 'LIN' },
+	{ name: 'Ouratite',        short: 'OUR' },
+	{ name: 'Quartz',          short: 'QTZ' },
+	{ name: 'Riccite',         short: 'RIC' },
+	{ name: 'Sadaryx',         short: 'SAD' },
+	{ name: 'Saldynium (Ore)', short: 'SAL' },
+	{ name: 'Savrilium',       short: 'SAV' },
+	{ name: 'Silicon',         short: 'SIL' },
+	{ name: 'Stileron',        short: 'STL' },
+	{ name: 'Taranite',        short: 'TAR' },
+	{ name: 'Tin',             short: 'TIN' },
+	{ name: 'Titanium',        short: 'TIT' },
+	{ name: 'Torite',          short: 'TOR' },
+	{ name: 'Tungsten',        short: 'TUN' },
+]
